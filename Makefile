a4b7a9c8c0350dd5b52413b83b475f64d6a0a57e
bil022 <bil022@ucsd.edu>
2021-11-19 07:25:41 -0800

    head

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ all:
 d:
 	npm run build
 rsync:
-	rsync -av renlab.sdsc.edu:/var/www/html/bil022/Finder/fastq.*.heads.gz meta/.
+	rsync -av renlab.sdsc.edu:/var/www/html/bil022/Finder/fastq.*.head.gz meta/.
+	(gzcat meta/fastq.*.head.gz | awk '{print $$1,$$NF}'; gzcat meta/fastq.20*.head.gz) | meta/head.pl > public/items.json
 	@rsync -av -q dist/* renlab.sdsc.edu:/var/www/html/bil022/Finder/v-finder/.
 	@echo http://renlab.sdsc.edu/Finder/v-finder/
--- meta/head.pl
+++ meta/head.pl
@@ -8,10 +8,22 @@ while (<DATA>) { chomp();
 
 while (<>) {
 	@data=split();
-	die $_ unless (@data==4 || @data==5);
-	($path, $size, $tm, $head, $tag)=@data;
-	die unless $path=~/^\/projects\/ps-renlab\/fastq\/(\S+)\/(\S+)$/;
-	($short_path, $file)=($1, $2);
+	if (@data==2) {
+		die "2:$_" unless /^(\S+)\/\S+\s(\d+)$/; ($path, $len)=($1,$2);
+		$LEN{$path}=$len unless exists $LEN{$path};
+		$LEN{$path}=$len if $len>$LEN{$path};
+		next;
+	}
+	die "A:$_" unless (@data==5 || @data==6); undef $tag;
+	($path, $size, $tm, $head, $seqlen)=@data if @data==5;
+	($path, $size, $tm, $head, $tag, $seqlen)=@data if @data==6;
+	die "a:$_" unless $path=~/^(\S+)\/\S+$/; $dir=$1;
+	#/mnt/transposon/fastq08-14/
+	if ($path=~/^\/projects\/ps-renlab\/fastq\/(\S+)\/(\S+)$/) {
+		($short_path, $file)=($1, $2); $drive="ps-renlab";
+	} elsif ($path=~/^\/mnt\/transposon\/fastq08-14\/(\S+)\/(\S+)$/) {
+		($short_path, $file)=($1, $2); $drive="transposon";
+	} else { die "B:$path"; }
 	if ($short_path=~/(\d\d\d\d)_(\d\d)_(\d\d)/) {
 		($y, $m, $d)=($1,$2,$3);
 	} elsif ($short_path=~/(\d\d\d\d)_(\d\d)(\d\d)_/) {
@@ -19,10 +31,10 @@ while (<>) {
 	} elsif ($short_path=~/_(\d\d)(\d\d)(20\d\d)_/) {
 		($y, $m, $d)=($3,$1,$2);
 	} else {
-		die $short_path;
+		die "C:$short_path";
 	}
-	die unless $d<=31 && $m<=12; $mdy="$m/$d/$y";
-    ## $short_path, $file, $mdy, $size
+	die "D:$_" unless $d<=31 && $m<=12; $mdy="$m/$d/$y";
+	## $short_path, $file, $mdy, $size
 
 	if ($tag) {
 		# https://kscbioinformatics.wordpress.com/2017/02/03/raw-illumina-sequence-data-files-for-dummies-part-1/
@@ -32,29 +44,31 @@ while (<>) {
 			# @VH00454:6:AAAKTJ3M5:1:1102:47676:10977:TGAGAGGCTA 
 			($machine, $rid, $fcid, $ln, $tile, $x, $y, $seq0)=($1,$2,$3,$4,$5,$6,$7,$8);
 		} else {
-			die $_;
+			die "E:$head";
 		}
-		die $_ unless $tag=~/^(\d):([YN]):(\d):(\S*)$/;
+		die "F:$_" unless $tag=~/^(\d):([YN]):(\d):(\S*)$/;
 		($pair, $filtered, $ctl, $idx)=($1,$2,$3,$4);
+		#@CTGACACAACGCTCTTTCAGAA:A00953:410:HKGVLDSX2:4:1101:1362:1000 1:N:0:4
+		$machine=$1 if $machine=~/^@[ACTGN]+:(\S+)$/;
         ## $machine, $rid, $fcid, $ln, $tile, $x, $y, [$seq0], $pair, $filtered, $ctl, $idx
 	} else {
 		# https://en.wikipedia.org/wiki/FASTQ_format
 		if ($head=~/^(\S+)_(\d+):(\d):(\d+):(\d+):(\d+)#(\S+)\/(\d)$/) {
 			($machine, $run, $ln, $tile, $x, $y, $idx, $pair)=($1,$2,$3,$4,$5,$6,$7,$8);
 		} elsif ($head=~/^(\S+)_(\d+):(\d):(\d+):(\d+):(\d+)$/) {
-            ($machine, $run, $ln, $tile, $x, $y)=($1,$2,$3,$4,$5,$6);
+            		($machine, $run, $ln, $tile, $x, $y)=($1,$2,$3,$4,$5,$6);
 		} else {
-			die $_;
+			die "F:$_";
 		}
         $fcid="${machine}_$run";
         ## $machine,   $run         $ln, $tile, $x, $y,         [$pair],                [$idx]
 	}
     ## $machine, $fcid_or_run
-    $key="$short_path.$fcid";
-    $run_info="\"path\": \"$short_path\", \"fcid\": \"$fcid\", \"mdy\": \"$mdy\", \"machine\": \"$machine\"";
+    $key="$short_path.$fcid"; die "1:$dir" unless exists $LEN{$dir}; $seqlen=$LEN{$dir};
+    $run_info="\"path\": \"$short_path\", \"fcid\": \"$fcid\", \"mdy\": \"$mdy\", \"machine\": \"$machine\", \"length\": \"$seqlen\", \"drive\": \"$drive\"";
     if (exists $INFO{$key}) {
         # make sure same: $path, $fcid, $date, $machine are the same
-        die $_ unless $run_info eq $INFO{$key};
+        warn "G: $key\n$run_info\n <> \n$INFO{$key}\n$_\n" unless $run_info eq $INFO{$key};
     } else {
         # add $short_path, $fcid, $date & $machine
         $INFO{$key}=$run_info;
@@ -69,8 +83,8 @@ while (<>) {
 }
 
 sub byDate {
-    die $INFO{$a} unless $INFO{$a}=~/"mdy": "(\d+)\/(\d+)\/(\d+)"/; ($ma,$da,$ya)=($1,$2,$3);
-    die $INFO{$b} unless $INFO{$b}=~/"mdy": "(\d+)\/(\d+)\/(\d+)"/; ($mb,$db,$yb)=($1,$2,$3);
+    die "H:$INFO{$a}" unless $INFO{$a}=~/"mdy": "(\d+)\/(\d+)\/(\d+)"/; ($ma,$da,$ya)=($1,$2,$3);
+    die "I:$INFO{$b}" unless $INFO{$b}=~/"mdy": "(\d+)\/(\d+)\/(\d+)"/; ($mb,$db,$yb)=($1,$2,$3);
     return $yb<=>$ya if $ya!=$yb;
     return $mb<=>$ma if $ma!=$mb;
     return $db<=>$da if $da!=$db;
